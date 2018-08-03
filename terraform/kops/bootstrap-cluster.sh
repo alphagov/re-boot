0ca454ea2a0a7a9a09cb11949c37a47bfadfe796
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2018-08-03 11:27:37 +0100

    Better bootstrapping step (dashboard, prometheus)
    - applies the kubernetes-dashboard addon
    - applies the prometheus-operator addon
    - script has instructions of how to connect

--- terraform/kops/bootstrap-cluster.sh
+++ terraform/kops/bootstrap-cluster.sh
@@ -7,21 +7,65 @@ function cluster_up () {
        --state=s3://gds-paas-k8s-shared-state &> /dev/null
 }
 
-echo 'Checking if cluster is ready'
+echo '🆗  Starting bootstrap procedure'
+echo '❓  Checking if cluster is ready'
 
 if ! cluster_up; then
-  echo 'Cluster not ready, exiting';
+  echo '‼️  Cluster not ready, exiting';
   exit 1
 fi
 
-echo 'Cluster is ready, proceeding'
+echo '✅  Cluster is ready, proceeding'
 
 cat <<MSG
-Installing untrusted software
-  - prometheus
+🔧  Installing untrusted software
+      - prometheus-operator
+      - kubernetes-dashboard
 MSG
 
 kubectl apply \
-  -f https://raw.githubusercontent.com/kubernetes/kops/master/addons/prometheus-operator/v0.19.0.yaml
+  -f https://raw.githubusercontent.com/kubernetes/kops/master/addons/prometheus-operator/v0.19.0.yaml \
+  -f https://raw.githubusercontent.com/kubernetes/kops/master/addons/kubernetes-dashboard/v1.8.3.yaml
 
-echo 'Done'
+service_account_name="default"
+
+kubectl apply -f <(cat <<EOF
+apiVersion: rbac.authorization.k8s.io/v1beta1
+kind: ClusterRoleBinding
+metadata:
+  name: ${service_account_name}-user-cluster-admin
+  labels:
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: cluster-admin
+subjects:
+- kind: ServiceAccount
+  name: ${service_account_name}
+  namespace: default
+EOF
+)
+
+service_account_secret_name="$(
+  kubectl get serviceaccount default -o jsonpath='{.secrets[0].name}'
+)"
+service_account_token="$(
+  kubectl get secret "$service_account_secret_name" -o jsonpath='{.data.token}' |\
+  base64 --decode
+)"
+
+cat <<EOF
+
+-------------------------------------------------------------------------------
+
+💻  In a separate terminal open a tunnel to the cluster with "kubectl proxy"
+
+🔑  You will be able to log in with the following token:
+
+$service_account_token
+
+-------------------------------------------------------------------------------
+
+EOF
+
+echo '✅  Finished bootstrap procedure'
