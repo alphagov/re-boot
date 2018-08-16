45d916fc01c6eacabce00f9378d6703ba12be267
Lee Porte <lee.porte@digital.cabinet-office.gov.uk>
2018-08-16 11:28:02 +0100

    fixup! DNS deployment

--- kops/deploy-dns.sh
+++ kops/deploy-dns.sh
@@ -20,12 +20,16 @@ fi
 kops export kubecfg "${cluster_name}.k8s.local" --state="${state_bucket}"
 
 kops replace --state=${state_bucket} -f  <( \
-	spruce merge \
-		<(kops get cluster ${cluster_name}.k8s.local --state=${state_bucket} --full -o yaml | grep -v \/\/) \
-		dns-iam.yaml \
+	spruce merge dns-iam.yaml \
+		<(kops get cluster ${cluster_name}.k8s.local --state=${state_bucket} -o yaml ) \
 )
 
 kops update cluster ${cluster_name}.k8s.local --state=${state_bucket} --yes
 kops rolling-update cluster ${cluster_name}.k8s.local --state=${state_bucket} --yes
+cp "${script_dir}/deploy-dns-pod.yaml" deploy-dns-pod-merged.yaml
+perl -pi -e s,--domain-filter=DNS,--domain-filter=$dns,g deploy-dns-pod-merged.yaml
+perl -pi -e s,--txt-owner-id=OWNER,--txt-owner-id=$owner_id,g deploy-dns-pod-merged.yaml
 
-kubectl apply -f  <(spruce merge "${script_dir}/deploy-dns.yaml")
+kubectl apply -f "${script_dir}/deploy-dns.yaml"
+kubectl apply -f  <(spruce merge "${script_dir}/deploy-dns-pod-merged.yaml")
+rm deploy-dns-pod-merged.yaml
