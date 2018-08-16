47b68ad16706d6ffe346c794ed7fa0afd5304049
Lee Porte <lee.porte@digital.cabinet-office.gov.uk>
2018-08-16 08:20:51 +0100

    fixup! DNS setup first attempt

--- kops/deploy-dns.sh
+++ kops/deploy-dns.sh
@@ -1,28 +1,31 @@
 #!/usr/bin/env bash
 set -ueo pipefail
 
-: "${GOOGLE_AUTH_CLIENT_ID}"
-
-export GOOGLE_AUTH_CLIENT_ID
-
-dns="$1"
-owner_id="aws route53 list-hosted-zones-by-name --dns-name "${dns}." | jq -r '.HostedZones[0].Id'"
+cluster_name="$1"
+dns="$2"
+owner_id=$(aws route53 list-hosted-zones-by-name --dns-name "${dns}." | jq -r '.HostedZones[0].Id')
 state_bucket='s3://gds-paas-k8s-shared-state'
 
 script_dir="$(pwd)"
 
-aws route53 create-hosted-zone --name "${dns}." --caller-reference "external-dns-test-$(date +%s)"
+currentdns="aws route53 list-hosted-zones-by-name | jq -r '.HostedZones[].Name'"
 
-kops export kubecfg "${cluster_name}" --state="${state_bucket}"
-kubectl apply -f "${script_dir}/mgmt/users.yaml"
+if [ "${owner_id}" != "null" ]; then
+    echo "Domain already exists"
+else
+    echo "Domain doesn't exist, creating"
+    aws route53 create-hosted-zone --name "${dns}." --caller-reference "external-dns-test-$(date +%s)"
+fi
+
+kops export kubecfg "${cluster_name}.k8s.local" --state="${state_bucket}"
 
 kops replace --state=${state_bucket} -f  <( \
 	spruce merge \
-		<(kops get cluster ${cluster_name} --state=${state_bucket} --full -o yaml | grep -v \/\/) \
+		<(kops get cluster ${cluster_name}.k8s.local --state=${state_bucket} --full -o yaml | grep -v \/\/) \
 		dns-iam.yaml \
 )
 
-kops update cluster ${cluster_name} --state=${state_bucket} --yes
-kops rolling-update cluster ${cluster_name} --state=${state_bucket} --yes
+kops update cluster ${cluster_name}.k8s.local --state=${state_bucket} --yes
+kops rolling-update cluster ${cluster_name}.k8s.local --state=${state_bucket} --yes
 
 kubectl apply -f  <(spruce merge "${script_dir}/deploy-dns.yaml")
