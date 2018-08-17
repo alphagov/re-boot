25122690102de1247106e441cc36e6cce28dd241
Rafal Proszowski <rafal.proszowski@digital.cabinet-office.gov.uk>
2018-08-17 12:23:13 +0100

    Add vault setup script

---
+++ kops/setup-vault.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+set -ueo pipefail
+
+## RUN IT ONLY ONCE
+
+kubectl -n default get vault vault-service -o jsonpath='{.status.vaultStatus.sealed[0]}' | xargs -0 -I {} kubectl -n default port-forward {} 8200
+export VAULT_ADDR='https://localhost:8200'
+export VAULT_SKIP_VERIFY="true"
+
+vault status
+vault operator init > vault-keys.txt
+vault operator unseal "$(cat vault-keys.txt | awk '/Unseal Key 1/ {print $NF}')"
+vault operator unseal "$(cat vault-keys.txt | awk '/Unseal Key 3/ {print $NF}')"
+vault operator unseal "$(cat vault-keys.txt | awk '/Unseal Key 5/ {print $NF}')"
+export VAULT_TOKEN="$(cat vault-keys.txt | awk '/Initial Root Token/ {print $NF}')"
+vault login "${VAULT_TOKEN}"
+
+kubectl -n default create serviceaccount vault-tokenreview
+kubectl create -f kops/mgmt/vault-kubernetes-auth.yaml
+export SECRET_NAME=$(kubectl -n default get serviceaccount vault-tokenreview -o jsonpath='{.secrets[0].name}')
+export TR_ACCOUNT_TOKEN=$(kubectl -n default get secret ${SECRET_NAME} -o jsonpath='{.data.token}' | base64 --decode)
+vault auth enable kubernetes
+cat ~/.kube/kubeconfig | awk '/certificate-authority-data/ {print $NF}' | base64 -D > ca.crt
+vault write auth/kubernetes/config kubernetes_host="$(kubectl config view --minify | awk '/server/ {print $NF}')" kubernetes_ca_cert=@ca.crt token_reviewer_jwt=$TR_ACCOUNT_TOKEN
+
+vault write sys/policy/concourse-policy policy=@vault-policies.hcl
+vault write auth/kubernetes/role/concourse-role \
+    bound_service_account_names=default \
+    bound_service_account_namespaces=default \
+    policies=concourse-policy \
+    ttl=1h
+
+export SECRET_NAME=$(kubectl -n default get serviceaccount default -o jsonpath='{.secrets[0].name}')
+export DEFAULT_ACCOUNT_TOKEN=$(kubectl -n default get secret ${SECRET_NAME} -o jsonpath='{.data.token}' | base64 --decode)
+vault write auth/kubernetes/login role=concourse-role jwt=${DEFAULT_ACCOUNT_TOKEN}
+
+echo "${DEFAULT_ACCOUNT_TOKEN}" > account-token.txt
