4c82ef6324b83bb937a16f4e8ce2154e68921c94
Lee Porte <lee.porte@digital.cabinet-office.gov.uk>
2018-08-20 13:15:46 +0100

    Fixed setup-vault script

--- kops/setup-vault.sh
+++ kops/setup-vault.sh
@@ -8,7 +8,7 @@ export VAULT_ADDR="https://localhost:8200"
 export VAULT_SKIP_VERIFY="true"
 
 sleep 30
-vault_status="$(curl http://127.0.0.1:8200/v1/sys/health)"
+vault_status="$(curl -k -s -o /dev/null -I -w "%{http_code}" -X GET https://127.0.0.1:8200/v1/sys/health)"
 if [ $vault_status == 501 ]; then
   vault operator init | tee vault-keys.txt
   vault operator unseal "$(cat vault-keys.txt | awk '/Unseal Key 1/ {print $NF}')"
@@ -23,12 +23,15 @@ if [ $vault_status == 503 ]; then
 fi
 vault login "$(cat vault-keys.txt | awk '/Initial Root Token/ {print $NF}')"
 
-kubectl -n default create serviceaccount vault-tokenreview
+CHECK_SERVICEACCOUNT=$(kubectl -n default get serviceaccount vault-tokenreview -o jsonpath='{.metadata.name}')
+if [ $CHECK_SERVICEACCOUNT != "vault-tokenreview" ]; then
+    kubectl -n default create serviceaccount vault-tokenreview
+fi
 kubectl create -f mgmt/vault-kubernetes-auth.yaml
 export SECRET_NAME=$(kubectl -n default get serviceaccount vault-tokenreview -o jsonpath='{.secrets[0].name}')
 export TR_ACCOUNT_TOKEN=$(kubectl -n default get secret ${SECRET_NAME} -o jsonpath='{.data.token}' | base64 --decode)
 vault auth enable kubernetes
-cat ~/.kube/kubeconfig | awk '/certificate-authority-data/ {print $NF}' | base64 -D > ca.crt
+cat ~/.kube/config | awk '/certificate-authority-data/ {print $NF}' | base64 -D > ca.crt
 vault write auth/kubernetes/config kubernetes_host="$(kubectl config view --minify | awk '/server/ {print $NF}')" kubernetes_ca_cert=@ca.crt token_reviewer_jwt=$TR_ACCOUNT_TOKEN
 vault mount -path=/concourse -description="Secrets for concourse pipelines" generic
 
